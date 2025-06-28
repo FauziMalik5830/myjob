0f2667e3a1cffb105f7970e11e96a26d9bb6799c
Fauzi malik <uzieahmad1002@gmail.com>
2025-06-29 02:10:25 +0800

    Update settings.py dan tambah requirements-dev.txt

--- myjob/settings.py
+++ myjob/settings.py
@@ -104,16 +104,10 @@ WSGI_APPLICATION = 'myjob.wsgi.application'
 #}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
-        'PORT': os.environ.get('MYSQLPORT'),
-    }
+    'default': dj_database_url.config(
+        default='mysql://root:@127.0.0.1:3306/artikel_blog'  # fallback ke XAMPP
+    )
 }
-
 
 
 
